0034de17e54802e12ca7d8db0f480262a8556c11
Eric Rovell <ericrovell@gmail.com>
2021-07-15 20:35:34 +0300

    feat: types index

---
+++ src/types/index.ts
@@ -0,0 +1,18 @@
+export type {
+  Color,
+  ColorObject,
+  ColorRGB,
+  ColorHEX,
+  ColorHSL,
+  ColorHSV,
+  ColorHSB,
+  ColorCMYK
+} from "./color";
+
+export { 
+  InputObject,
+  Input,
+  Parser,
+  Parsers,
+  ParseResult
+} from "./parser";
