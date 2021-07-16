bab9687ee1ad891622ed10dc37e9967be2c7047c
Eric Rovell <ericrovell@gmail.com>
2021-07-16 15:49:32 +0300

    add: cmyk model parsers

---
+++ src/models/cmyk/parse.ts
@@ -0,0 +1,23 @@
+import { clampCMYK } from "./util";
+import { cmyk2rgb } from "./transform";
+import { checkValue } from "@util/helpers";
+import type { ColorRGB, InputObject } from "@types";
+
+/**
+ * Parses the CMYK color object into RGB.
+ */
+export function parseCMYKColor({ c, m, y, k, a = 1 }: InputObject): ColorRGB | null {
+  if (checkValue(c) && checkValue(m) && checkValue(y) && checkValue(k)) {
+    const cmyk = clampCMYK({
+      c: Number(c),
+      m: Number(m),
+      y: Number(y),
+      k: Number(k),
+      a: Number(a)
+    });
+    
+    return cmyk2rgb(cmyk);
+  }
+  
+  return null;
+}
