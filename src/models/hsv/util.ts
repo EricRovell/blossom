4183d7f6ff8c6c5105e6edba1b0c8cc82dbfbc8d
Eric Rovell <ericrovell@gmail.com>
2021-07-16 15:44:39 +0300

    add: hsv utility functions

---
+++ src/models/hsv/util.ts
@@ -0,0 +1,26 @@
+import { clamp, clampDegrees, round } from "@util/helpers";
+import type { ColorHSV } from "@types";
+
+/**
+ * Clamps the HSV color object values.
+ */
+export function clampHSV({ h, s, v, a = 1 }: ColorHSV): ColorHSV {
+  return {
+    h: clampDegrees(h),
+    s: clamp(s, 0, 100),
+    v: clamp(v, 0, 100),
+    a: clamp(a)
+  }
+};
+
+/**
+ * Rounds the HSV color object values.
+ */
+export function roundHSV({ h, s, v, a = 1 }: ColorHSV): ColorHSV {
+  return {
+    h: round(h),
+    s: round(s, 1),
+    v: round(v, 1),
+    a: round(a)
+  };
+}
