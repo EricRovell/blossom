1cb092739ab620e697c2358c1861d221f0424db2
Eric Rovell <ericrovell@gmail.com>
2021-07-17 17:29:52 +0300

    fix: clamp the values properly

--- src/models/hsv/parse.ts
+++ src/models/hsv/parse.ts
@@ -1,15 +1,21 @@
 import { hsv2rgb } from "./transform";
 import { clampHSV } from "./util";
 import { checkValue } from "@util/helpers";
-import { ColorRGB, ColorHSV, InputObject } from "@types";
+import { ColorRGB, InputObject } from "@types";
 
 /**
  * Parses the HSV color object into RGB.
  */
 export function parseHSVColor({ h, s, v, a = 1 }: InputObject): ColorRGB | null {
   if (checkValue(h) && checkValue(s) && checkValue(v)) {
-    const hsv = { h, s, v, a } as ColorHSV;
-    return hsv2rgb(clampHSV(hsv))
+    const hsv = clampHSV({
+      h: Number(h),
+      s: Number(s),
+      v: Number(v),
+      a: Number(a)
+    });
+
+    return hsv2rgb(hsv);
   }
   
   return null;
