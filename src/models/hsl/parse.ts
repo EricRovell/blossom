7f35c710105bae21f5a2287a659da7db2611d525
Eric Rovell <ericrovell@gmail.com>
2021-07-17 17:29:29 +0300

    fix: clamp the values for correct output

--- src/models/hsl/parse.ts
+++ src/models/hsl/parse.ts
@@ -17,12 +17,12 @@ export function parseHSLString(input: string): ColorRGB | null {
     return null;
   }
   
-  const hsl = {
+  const hsl = clampHSL({
     h: parseHue(match[1], match[2]),
     s: Number(match[3]),
     l: Number(match[4]),
     a: match[5] === undefined ? 1 : Number(match[5]) / (match[6] ? 100 : 1)
-  };
+  });
   
   return hsl2rgb(hsl);
 }
