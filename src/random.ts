9c772f8962923a83c13f0affa399aa1c035f3095
Eric Rovell <ericrovell@gmail.com>
2021-07-17 17:37:14 +0300

    add: random color utility

---
+++ src/random.ts
@@ -0,0 +1,12 @@
+import { Blossom } from "./blossom";
+
+/**
+ * Creates an instance with random color.
+ */
+export function random(): Blossom {
+  return new Blossom({
+    r: Math.random() * 255,
+    g: Math.random() * 255,
+    b: Math.random() * 255
+  });
+}
