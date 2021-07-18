0b39c255d063040d90212692a8bcfc2ffc9c9157
Eric Rovell <ericrovell@gmail.com>
2021-07-18 20:39:36 +0300

    upd: moved exports for lib consumers

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+export { Blossom, blossom } from "./blossom";
+export { getModel } from "./properties"
+export { random } from "./random";
+
+export type {
+  ColorModel,
+  Color,
+  ColorRGB,
+  ColorHEX,
+  ColorHSL,
+  ColorHSV,
+  ColorCMYK,
+} from "./types/color";
