c9870be7ee6b86483507d739aabbc413117d98aa
Eric Rovell <ericrovell@gmail.com>
2021-07-25 13:38:57 +0300

    upd: monochromatic plugin tests

--- tests/plugins.test.ts
+++ tests/plugins.test.ts
@@ -1,6 +1,7 @@
 import { blossom } from "@/blossom";
 import { extend } from "@/extend";
 import { pluginHarmonies } from "@plugins/harmonies";
+import { pluginMonochromatic } from "@plugins/monochromatic";
 
 describe("Harmony colors plugin", () => {
   extend([ pluginHarmonies ]);
@@ -36,5 +37,69 @@ describe("Harmony colors plugin", () => {
     expect(color.harmonies("splitcomplimentary").map(value => value.hex)).toEqual([
       "#FF0000", "#00FF80", "#0080FF"
     ]);
+  });
+});
+
+describe("Monochromatic colors plugin", () => {
+  extend([ pluginMonochromatic ]);
+
+  it("Generates tints", () => {
+    expect(blossom("#ff0000").tints(10).map(color => color.hex)).toEqual([
+      "#FF0000",
+      "#FF1A1A",
+      "#FF3333",
+      "#FF4D4D",
+      "#FF6666",
+      "#FF8080",
+      "#FF9999",
+      "#FFB3B3",
+      "#FFCCCC",
+      "#FFE5E5",
+      "#FFFFFF"
+    ]);
+  });
+
+  it("Generates shades", () => {
+    expect(blossom("#ff0000").shades(10).map(color => color.hex)).toEqual([
+      "#FF0000",
+      "#E60000",
+      "#CC0000",
+      "#B30000",
+      "#990000",
+      "#800000",
+      "#660000",
+      "#4D0000",
+      "#330000",
+      "#190000",
+      "#000000"
+    ]);
+  });
+
+  it("Generates tones", () => {
+    expect(blossom("#ff0000").tones(10).map(color => color.hex)).toEqual([
+      "#FF0000",
+      "#F20D0D",
+      "#E61919",
+      "#D92626",
+      "#CC3333",
+      "#BF4040",
+      "#B34D4D",
+      "#A65959",
+      "#996666",
+      "#8C7373",
+      "#808080"
+    ]);
+  });
+
+  it("Won't generate tints for pure white color", () => {
+    expect(blossom("#FFF").tints().map(color => color.hex)).toEqual([]);
+  });
+
+  it("Won't generate shades for pure black color", () => {
+    expect(blossom("#000").shades().map(color => color.hex)).toEqual([]);
+  });
+
+  it("Won't generate tones for pure gray color", () => {
+    expect(blossom("#808080").tones().map(color => color.hex)).toEqual([]);
   });
 });
