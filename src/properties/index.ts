c36ef5a24d1aef044458fb2383b58ea67a4940c4
Eric Rovell <ericrovell@gmail.com>
2021-09-03 16:57:16 +0300

    create color distance functions

--- src/properties/index.ts
+++ src/properties/index.ts
@@ -3,3 +3,4 @@ export { getModel } from "./model";
 export { getLuminance } from "./luminance";
 export { getContrast } from "./contrast";
 export { cssColorNames } from "./css-color-names";
+export { getDistance, getDistanceSq } from "./distance";
