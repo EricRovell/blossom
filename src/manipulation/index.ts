ed90a47ecba6f58a7f7829cb92eef1c3f1b5680d
Eric Rovell <ericrovell@gmail.com>
2021-09-04 15:17:26 +0300

    create nearest color helper

--- src/manipulation/index.ts
+++ src/manipulation/index.ts
@@ -2,3 +2,4 @@ export { invert } from "./invert";
 export { lighten } from "./lighten";
 export { saturate } from "./saturate";
 export { mix } from "./mix";
+export { findNearestColor } from "./nearest-color";
