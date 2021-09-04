a1c860f7c70bc423f94ffcc8ff75be56cc182716
Eric Rovell <ericrovell@gmail.com>
2021-09-04 15:18:45 +0300

    names plugin final implementation

--- src/plugins/names.ts
+++ src/plugins/names.ts
@@ -1,21 +1,99 @@
+import { findNearestColor } from "@manipulation";
+import { hex2rgb } from "@models/hex";
 import { cssColorNames } from "@properties";
 import { ColorRGB, Plugin } from "../types";
+
+type NamesRGBCache = Map<string, ColorRGB>;
+type HexNamesCache = Map<string, string>;
+
+declare module "../blossom" {
+	interface Blossom {
+		/**
+		 * Returns match to the named CSS color if possible.
+		 */
+		name: string | null;
+
+		/**
+		 * Returns a closest match to the named CSS color.
+		 */
+		closestName: string;
+	}
+}
+
+/**
+ * Cache CSS color names as Map:
+ *  - name -> rgb (distance calculations optimixation);
+ */
+function createNamesRGBCache(): NamesRGBCache {
+	const names2RGBMap = new Map();
+
+	for (const [ name, hex ] of Object.entries(cssColorNames)) {
+		names2RGBMap.set(name, hex2rgb(hex));
+	}
+
+	return names2RGBMap;
+}
+
+/**
+ * Cache CSS names as:
+ * 	- hex -> name pair for faster search by hex;
+ */
+function createHexNamesCache(): HexNamesCache {
+	const hex2namesMap = new Map();
+
+	for (const [ name, hex ] of Object.entries(cssColorNames)) {
+		hex2namesMap.set(hex, name);
+	}
+
+	return hex2namesMap;
+}
 
 /**
  * Adds support for [named CSS colors](https://www.w3.org/TR/css-color-4/#named-colors).
  */
 export const pluginNames: Plugin = (BaseClass, parsers) => {
+	let cacheRGB: NamesRGBCache;
+	let cacheHEX: HexNamesCache;
+
+	Object.defineProperty(BaseClass.prototype, "name", {
+		get: function name() {
+			if (!this.color.a) {
+				return "transparent";
+			}
+
+			if (!cacheHEX) {
+				cacheHEX = createHexNamesCache();
+			}
+
+			return cacheHEX.get(this.hex.toLowerCase()) ?? null;
+		}
+	});
+
+	Object.defineProperty(BaseClass.prototype, "closestName", {
+		get: function closestName() {
+			const exact = this.name;
+
+			if (exact) {
+				return exact;
+			}
+
+			if (!cacheRGB) {
+				cacheRGB = createNamesRGBCache();
+			}
+
+			return findNearestColor(this.rgb, cacheRGB);
+		}
+	});
+
 	/**
 	 * Parses CSS color name.
 	 */
 	function parseColorName(input: string): ColorRGB | null {
-		const name = input.toLowerCase();
+		const hex = cssColorNames[input.toLowerCase()];
 
-		if (cssColorNames.has(name)) {
-			return new BaseClass(`#${cssColorNames.get(name)}`).rgb;
-		}
-
-		return null;
+		return hex
+			? new BaseClass(hex).rgb
+			: null;
 	}
 
 	parsers.string.push({
