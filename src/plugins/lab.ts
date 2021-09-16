41aad4f482a40bad8801804518dee8bd31273ada
Eric Rovell <ericrovell@gmail.com>
2021-09-16 23:26:12 +0300

    update lab plugin, include deltaE2000

--- src/plugins/lab.ts
+++ src/plugins/lab.ts
@@ -1,7 +1,8 @@
 import { rgb2lab } from "@models/rgb";
 import { parseLABColor, roundLAB } from "@models/lab";
 import { rgb2labString } from "@models/rgb";
-import type { ColorLAB, Plugin } from "../types";
+import { deltaE2000 } from "@properties";
+import type { Input, ColorLAB, Plugin } from "../types";
 
 declare module "../blossom" {
 	interface Blossom {
@@ -16,6 +17,12 @@ declare module "../blossom" {
 		 * https://developer.mozilla.org/en-US/docs/Web/CSS/color_value/lab()
 		 */
 		toStringLAB: string;
+
+		/**
+		 * Calculates the perceived color difference for two colors according to
+		 * [Delta E2000](https://en.wikipedia.org/wiki/Color_difference#CIEDE2000).
+		 */
+		deltaE00(color: Blossom | Input): number;
 	}
 }
 
@@ -36,6 +43,14 @@ export const pluginLAB: Plugin = (BaseClass, parsers) => {
 			return rgb2labString(this.color);
 		}
 	});
+
+	BaseClass.prototype.deltaE00 = function(color) {
+		const compared = color instanceof BaseClass
+			? color
+			: new BaseClass(color);
+
+		return deltaE2000(this.lab, compared.lab);
+	};
 
 	parsers.object.push({
 		model: "lab",
