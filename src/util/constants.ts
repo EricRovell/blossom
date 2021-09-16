343ca018e0397d547e9559fe67a70d4f18bc0883
Eric Rovell <ericrovell@gmail.com>
2021-09-16 23:25:53 +0300

    add radian/degrees constants

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -6,3 +6,13 @@ export const ANGLE_UNITS: Record<string, number> = {
 	turn: 360,
 	rad: 360 / (Math.PI * 2)
 };
+
+/**
+ * Multiplier that converts radians to degrees.
+ */
+export const rad2deg = 180 / Math.PI;
+
+/**
+ * Multiplier that converts degrees to radians.
+ */
+export const deg2rad = Math.PI / 180;
